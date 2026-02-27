175650737cc7c9aa31a880ab2e47a39c11a7b13c
Masashi Nishiwaki <m.tsuji1119@gmail.com>
2026-02-27 16:12:37 +0900

    Update claude-watch to 0.0.4

--- Casks/claude-watch.rb
+++ Casks/claude-watch.rb
@@ -1,6 +1,6 @@
 cask "claude-watch" do
-  version "0.0.3"
-  sha256 "78bf8c1821f2f650cec4617555c267fe3448f7558c4c00d873aec3d9eb908593"
+  version "0.0.4"
+  sha256 "b1da135078c8556a50f70d615098cc2b39aba89c0be2f0aa36e2592fcca5d5f2"
 
   url "https://github.com/htz/claude-watch/releases/download/v#{version}/claude-watch-darwin-arm64-#{version}.zip"
   name "Claude Watch"
